52a5ef086000327f9adce32144b4a8dee4879b44
pavanraparthi <pavan.raparthi@gmail.com>
2019-08-17 17:24:54 +0530

    2 files modified

--- components/header.js
+++ components/header.js
@@ -3,8 +3,8 @@ import React, {  Component } from 'react';
 const NonLoginTemplate = (props) => {
   return (
     <div>
-      <input type="button" value="Log in" onClick="{this.props.loginEvent}"/>
-      <input type="button" value="Sign Up" onClick="{this.props.signUpEvent}"/>
+      <input type="button" value="Log in" onClick={props.loginEvent}/>
+      <input type="button" value="Sign Up" onClick={props.signUpEvent}/>
     </div>
   )
 }
@@ -12,24 +12,26 @@ const NonLoginTemplate = (props) => {
 const LoginTemplate = (props) => {
   return (
     <div>
-      <span>{this.props.userName}</span>
-      <span>{this.props.lastUpdated}</span>
-      <input type="button" value="Log out" onClick="{this.props.logoutEvent}"/>
+      <span>{props.userName}</span>
+      <span>{props.lastUpdated}</span>
+      <input type="button" value="Log out" onClick={props.logoutEvent}/>
     </div>
   )
 }
 
 class Header extends Component {
-  constuctor (props){
+  constructor (){
     
   }
   render() {
     return (
       <div>
         <h1>Welcome to Todo App</h1>
-        { this.props.isLoggedIn ? <LoginTemplate userName="{this.props.userName}"
-        lastUpdated="{this.props.lastUpdated}" logoutEvent="{this.props.logoutEvent}"/> : <NonLoginTemplate loginEvent="{this.props.loginEvent}" signUpEvent="{this.props.signUpEvent}"/>}
+        { props.isLoggedIn ? <LoginTemplate userName={props.userName}
+        lastUpdated={props.lastUpdated} logoutEvent={props.logoutEvent}/> : <NonLoginTemplate loginEvent={props.loginEvent} signUpEvent={props.signUpEvent}/>}
       </div>
     )
   }
 }
+
+export default Header;
--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import './style.css';
+import Header from './components/header';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       todos: [],
+      userName:"",
       lastUpdated:new Date().toDateString()
     };
+  }
+
+  doLogout = () => {
+    console.log("do Logout");
+  }
+
+  doLogin = () => {
+    console.log("do Login");
+  }
+
+  doSignUp = () => {
+    console.log("do Signup");
   }
 
   render() {
     return (
       <div>
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+        <Header isLoggedIn={this.state.isLoggedIn}
+        userName={this.state.userName}
+        lastUpdated={this.state.lastUpdated}
+        logoutEvent={this.doLogout}
+        loginEvent={this.doLogin}
+        signUpEvent={this.doSignUp}></Header>
       </div>
     );
   }
